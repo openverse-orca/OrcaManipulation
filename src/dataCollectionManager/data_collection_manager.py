12f686752794565c2553d682ff653e932d78d0b8
yechaojun <yechaojcn@aliyun.com>
2026-03-10 20:20:41 +0800

    调整场景 UI 文本接口参数顺序并在任务切换时显示任务描述，提升数据采集过程中的实时提示与可观察性。

--- src/dataCollectionManager/data_collection_manager.py
+++ src/dataCollectionManager/data_collection_manager.py
@@ -1,4 +1,5 @@
 import enum
+from textwrap import shorten
 import time
 import numpy as np
 import gymnasium as gym
@@ -191,6 +192,7 @@ class DataCollectionManager:
                     self.scene_manager.update_actor_qpos()
                     self.task.get_task(self.scene_manager)
                     orca_logger.info(f"Task description: {self.task.get_task_description()}")
+                    self.scene_manager.set_ui_message(1, self.task.get_task_description(),showtime=10,blinkframe=5)
 
                 
             elif self.mode == self.DataCollectionMode.AUGMENTATION:
--- src/scene/scene_manager.py
+++ src/scene/scene_manager.py
@@ -234,7 +234,7 @@ class SceneManager:
         self._scene.get_rundata(scriptname,scenestage)
  
     def show_ui_message(self,actor_name: int =1, message: str = "",color: str = "0xffff00",blinkframe:int = 0,showtime:int = 0,size:int = 32):
-        self._scene.set_ui_text(actor_name, message, color, blinkframe, showtime, size)
+        self._scene.set_ui_text(actor_name, message, showtime,blinkframe,color,size)
 
     def show_ui_icon(self,actor: int =1, show: bool = True):
         self._scene.set_image_enabled(actor,show)
