420b3c3960b58f02ff549e3afd023eadc9da8430
agasea <against_sea@163.com>
2026-04-08 18:17:20 +0800

    添加replay脚本

--- src/examples/dataCollection/data_collection_aug.py
+++ src/examples/dataCollection/data_collection_aug.py
@@ -9,7 +9,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-sys.path.append(os.path.join("/home/orca/Projects/", "OrcaGym"))
+# sys.path.append(os.path.join("/home/orca/Projects/", "OrcaGym"))
 
 from devices.data_device import DataDevice
 from scene.scene_manager import SceneManager
--- src/examples/dataCollection/data_collection_replay.py
+++ src/examples/dataCollection/data_collection_replay.py
@@ -4,23 +4,24 @@ import sys
 import time
 import traceback
 
+
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
+
 # sys.path.append(os.path.join("/home/orca/Projects/", "OrcaGym"))
 
 
+from devices.data_device import DataDevice
 from scene.scene_manager import SceneManager
-from task.abstract_task import EmptyTask
 from task.pick_place_task import PickPlaceTask
-from devices.abstract_device import PicoJoystickDevice
-from orca_gym.devices.pico_joytsick import PicoJoystick, PicoJoystickKey
-from orca_gym.environment.orca_gym_local_env import OrcaGymLocalEnv
+from task.abstract_task import EmptyTask
 from orca_gym.log.orca_log import get_orca_logger, OrcaLog
 import numpy as np
 from dataCollectionManager.data_collection_manager import DataCollectionManager
 from controllers import controllers
 from yaml import load, Loader
+from devices.Interpolator.abstract_interpolator import OpenLoongInterpolator
 
 ENTRY_POINT = "envs.dataCollection.dataCollection_env:DataCollectionEnv"
 
@@ -33,24 +34,26 @@ orca_logger = get_orca_logger(name="DataCollection",
                               max_bytes=10*1024*1024, 
                               backup_count=5, 
                               console_level="INFO", 
-                              file_level="INFO",
+                              file_level="DEBUG",
                               log_dir=log_dir,
                               use_colors=True,
                               force_reinit=True)
 
 
 def main():
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--level", type=str, required=True, help="场景的名称")
-    parser.add_argument("--agent_name", type=str, required=True, choices=["openloong", "tiangong2"], help="机器人型号")
+    parser.add_argument("--agent_name", type=str, required=True, choices=["openloong","tiangong2"], help="机器人型号")
     parser.add_argument("--task_config", type=str, required=True, help="任务配置文件")
+    parser.add_argument("--replay_mode", type=str, default="osc", choices=["osc", "ik", "position"], 
+                        help="回放数据模式， osc, ik使用末端位置轨迹进行回放， position使用位置控制器数值进行回放")
 
     args = parser.parse_args()
 
     level = args.level
     agent_name = args.agent_name
     task_config = args.task_config
+    replay_mode = args.replay_mode
 
     orca_logger.info(f"log file: {log_file}")
     orca_logger.info(f"log dir: {log_dir}")
@@ -62,12 +65,8 @@ def main():
 
     if agent_name == "openloong":
         from conf import openloong_conf as agent_conf
-        from dataStorage.openloong_data_storage import OpenLoongDataStorage
-        data_storage = OpenLoongDataStorage(dataset_path=os.path.join(base_dir, "dataset", agent_name, level), hdf5_path="record/proprio_stats.hdf5")
     elif agent_name == "tiangong2":
         from conf import tiangong2_conf as agent_conf
-        from dataStorage.tiangong_data_storage import Tiangong2DataStorage
-        data_storage = Tiangong2DataStorage(dataset_path=os.path.join(base_dir, "dataset", agent_name, level), hdf5_path="record/proprio_stats.hdf5")
     else:
         raise ValueError(f"Invalid agent name: {agent_name}")
 
@@ -75,22 +74,18 @@ def main():
         default_joint_values[joint_name] = value
     for joint_name, value in zip(agent_conf.r_arm["joint_names"], agent_conf.r_arm["neutral_joint_values"]):
         default_joint_values[joint_name] = value
-    
+
     orca_logger.info("Creating device")
-    pico_joystick_device = PicoJoystickDevice(PicoJoystick())
+    data_device = DataDevice(os.path.join(base_dir, "aug_dataset", agent_name, level), "record/proprio_stats.hdf5")
 
     orca_logger.info("Creating scene manager")
     with open(os.path.join(base_dir, task_config), "r", encoding="utf-8") as f:
         config = load(f, Loader=Loader)
     scene_manager = SceneManager(orcagym_addr, config=config)
 
-    script_name = os.path.basename(sys.argv[0]) if sys.argv else os.path.basename(__file__)
-    scene_manager.show_ui_message(1, "开始仿真程序，请按左右遥杆进行操作 ", "0xffff00", showtime=10)
 
-    scene_manager.get_scene_data(script_name, "beginscene")
-
-    orca_logger.info("Creating data storage")
-    data_storage.set_video_path("video")
+    def obs_callback(env) -> dict:
+        return {"replay": np.zeros(env.nu, dtype=np.float32)}
 
     orca_logger.info("Creating data collection manager")
     data_collection_manager = DataCollectionManager(
@@ -98,37 +93,52 @@ def main():
         env_name=env_name,
         entry_point=ENTRY_POINT,
         default_joint_values=default_joint_values,
-        obs_callback=data_storage.obs_callback,
+        obs_callback=obs_callback,
         env_index=env_index,
-        device=pico_joystick_device,
+        device=data_device,
         scene_manager=scene_manager,
-        data_storage=data_storage,
+        data_storage=None,
         frame_skip=5,
     )
     env = data_collection_manager.env
     env.reset()
 
-    orca_logger.info("Disabling position controller")
-    data_collection_manager.set_disable_actuator_group([agent_conf.positions_group])
+    data_collection_manager.mode = DataCollectionManager.DataCollectionMode.AUGMENTATION
+    data_collection_manager.save_video = True
 
-    orca_logger.info("Creating left gripper controller")
-    controllers.add_gripper_2f85_pico_controller(data_collection_manager, env, agent_conf.gripper_l, agent_conf.base_body, pico_joystick_device, [PicoJoystickKey.X, PicoJoystickKey.Y, PicoJoystickKey.L_TRIGGER])
-    
-    orca_logger.info("Creating right gripper controller")
-    controllers.add_gripper_2f85_pico_controller(data_collection_manager, env, agent_conf.gripper_r, agent_conf.base_body, pico_joystick_device, [PicoJoystickKey.A, PicoJoystickKey.B, PicoJoystickKey.R_TRIGGER])
-    
-    orca_logger.info("Creating left arm controller")
-    controllers.add_arm_osc_pico_controller(data_collection_manager, env, agent_conf.l_arm, agent_conf.base_body, pico_joystick_device, PicoJoystickKey.L_TRANSFORM)
-    
-    orca_logger.info("Creating right arm controller")
-    controllers.add_arm_osc_pico_controller(data_collection_manager, env, agent_conf.r_arm, agent_conf.base_body, pico_joystick_device, PicoJoystickKey.R_TRANSFORM)
-    
+    if replay_mode == "osc":
+        orca_logger.info("Disabling position controller")
+        data_collection_manager.set_disable_actuator_group([agent_conf.positions_group])
+        controllers.add_arm_osc_openloong_data_controller(data_collection_manager, env, agent_conf.l_arm, agent_conf.base_body, data_device, left_arm=True)
+        controllers.add_arm_osc_openloong_data_controller(data_collection_manager, env, agent_conf.r_arm, agent_conf.base_body, data_device, left_arm=False)
+    elif replay_mode == "ik":
+        controllers.add_arm_ik_data_controller(data_collection_manager, env, agent_conf.l_arm, agent_conf.base_body, data_device, left_arm=True)
+        controllers.add_arm_ik_data_controller(data_collection_manager, env, agent_conf.r_arm, agent_conf.base_body, data_device, left_arm=False)
+    elif replay_mode == "position":
+        controllers.add_arm_position_data_controller(data_collection_manager, env, agent_conf.l_arm, agent_conf.base_body, data_device, left_arm=True)
+        controllers.add_arm_position_data_controller(data_collection_manager, env, agent_conf.r_arm, agent_conf.base_body, data_device, left_arm=False)
+    else:
+        raise ValueError(f"Invalid replay mode: {replay_mode}")
+
+    if agent_name == "openloong":
+        orca_logger.info("Creating left gripper controller")
+        controllers.add_gripper_2f85_openloong_data_controller(data_collection_manager, env, agent_conf.gripper_l, agent_conf.base_body, data_device, left_gripper=True)
+
+        orca_logger.info("Creating right gripper controller")
+        controllers.add_gripper_2f85_openloong_data_controller(data_collection_manager, env, agent_conf.gripper_r, agent_conf.base_body, data_device, left_gripper=False)
+    elif agent_name == "tiangong2":
+        orca_logger.info("Creating left gripper controller")
+        controllers.add_gripper_hand_data_controller(data_collection_manager, env, agent_conf.gripper_l, agent_conf.base_body, data_device, left_gripper=True)
+
+        orca_logger.info("Creating right gripper controller")
+        controllers.add_gripper_hand_data_controller(data_collection_manager, env, agent_conf.gripper_r, agent_conf.base_body, data_device, left_gripper=False)
+    else:
+        raise ValueError(f"Invalid agent name: {agent_name}")
+
     orca_logger.info("Creating pick place task")
     data_collection_manager.set_task(PickPlaceTask(env))
-    controllers.add_task_status_pico_controller(data_collection_manager, env, pico_joystick_device, agent_conf.base_body)
+    controllers.add_task_status_openloong_data_controller(data_collection_manager, env, data_device, agent_conf.base_body)
 
-    data_collection_manager.save_video = True
-    
     data_collection_manager.run()
 
 if __name__ == "__main__":
